25d65690b8a5c95f7d1bc3160feb9de9eb46a6ca
Ken Nishimura <ken97531@gmail.com>
2013-03-22 21:25:13 +0900

    html redesign

--- src/screen.js
+++ src/screen.js
@@ -1,6 +1,6 @@
 grap.Screen = {
     shim: 7,       // defines px between cards
-    width: 380,
+    width: 300,
     height: 0,     // will be calculated from the width
     deckSpace: 0,  // px to make some room to draw decks
     openDecks: [[], [], [], [], []],
